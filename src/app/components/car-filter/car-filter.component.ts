73a72c7a7cc087460abbb373d27f318d5673b7da
Anıl Büyükaşık <buyukasik.anil@gmail.com>
2021-04-02 08:39:31 +0300

    02.04

--- src/app/components/car-filter/car-filter.component.ts
+++ src/app/components/car-filter/car-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Brand } from 'src/app/models/brand';
 import { Color } from 'src/app/models/color';
 import { BrandService } from 'src/app/services/brand.service';
@@ -7,22 +8,25 @@ import { ColorService } from 'src/app/services/color.service';
 @Component({
   selector: 'app-car-filter',
   templateUrl: './car-filter.component.html',
-  styleUrls: ['./car-filter.component.css']
+  styleUrls: ['./car-filter.component.css'],
 })
 export class CarFilterComponent implements OnInit {
+  brands: Brand[];
+  selectedBrand: number;
+  allBrands: Brand;
+  brandFilterText: string;
 
-  brands:Brand[];
-  selectedBrand:Brand;
-  allBrands:Brand={brandId:0,brandName:""};
-  brandFilterText:string;
+  colors: Color[];
+  selectedColor: number;
+  allColors: Color;
+  colorFilterText: string;
 
-
-  colors:Color[];
-  selectedColor:Color;
-  allColors:Color
-  colorFilterText:string;
-
-  constructor(private brandService:BrandService, private colorService:ColorService) { }
+  constructor(
+    private brandService: BrandService,
+    private colorService: ColorService,
+    private activatedRoute:ActivatedRoute,
+    private router:Router
+  ) {}
 
   ngOnInit(): void {
     this.getBrands();
@@ -34,55 +38,45 @@ export class CarFilterComponent implements OnInit {
       this.brands = response.data;
     });
   }
-  getColors(){
-    this.colorService.getColors().subscribe(response=>{
-      this.colors=response.data
+  getColors() {
+    this.colorService.getColors().subscribe((response) => {
+      this.colors = response.data;
     });
   }
-  setCurrentBrand(brand:Brand){
-      this.selectedBrand=brand;
-      console.log(brand.brandId)
-   
+  allBrandsSelected() {
+    return this.selectedBrand == undefined ? true : false;
   }
-  setCurrentColor(color:Color){
-      this.selectedColor = color;
-   
-  }
-  allBrandsSelected(){
-    return this.selectedBrand == undefined ? true:false;
-  }
-  allColorsSelected(){
-    return this.selectedColor == undefined ? true:false;
-  }
-  // currentBrandId(event: any) {
-  //   this.brandId=event;
-  // }
-
-  // currentColorId(event: any) {
-  //   this.colorId=event;
-  // }
-
-  getSelectedBrand(brand: Brand) {
-    if (this.selectedBrand == brand) {
-      return "selected";
-      console.log(brand.brandId)
-    }
-    else {
-      return "";
-    }
+  allColorsSelected() {
+    return this.selectedColor == undefined ? true : false;
   }
 
-  getSelectedColor(color: Color) {
-    if (this.selectedColor == color) {
-      return "selected";
-    }
-    else {
-      return "";
-    }
-  }
-  selectChangeHandler (event: any) {
+  selectCurrentBrand(event: any) {
     //update the ui
     this.selectedBrand = event.target.value;
     console.log(this.selectedBrand);
-}
+  }
+  selectCurrentColor(event: any) {
+    //update the ui
+    this.selectedColor = event.target.value;
+    console.log(this.selectedColor);
+  }
+  setFilter(){
+    if(this.selectedBrand != undefined && this.selectedColor != undefined){
+      this.router.navigate([
+        'cars/brand/'+this.selectedBrand+'/color/'+this.selectedColor
+      ]);
+    }
+    else if(this.selectedBrand != undefined && this.selectedColor == undefined){
+      this.router.navigate(['cars/brand/'+this.selectedBrand]);
+    }
+    else if(this.selectedBrand == undefined && this.selectedColor != undefined){
+      this.router.navigate(['cars/color/'+this.selectedColor]);
+    }
+    else{
+      this.router.navigate(['cars/']);
+    }
+  }
+  clearFilter(){
+    this.router.navigate(['cars/']);
+  }
 }
